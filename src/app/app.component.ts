47d3e73c5a4d2c79ba18801d4dfb027390c3439e
bengeendokter <benifit.arts@gmail.com>
2025-02-04 21:44:50 +0100

    Implement structure logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,13 @@ import { setThemeFromHexColor } from 'm3-css-color-token-generator';
 })
 export class AppComponent
 {
+  public hexThemeColor: string = '#ff0000';
+  public PALETTES = ['primary', 'secondary', 'tertiary', 'neutral', 'neutral-variant', 'error'] as const;
+  public VALUES = [0, 4, 6, 10, 12, 17, 20, 22, 24, 30, 40, 50, 60, 70, 80, 87, 90, 92, 94, 95, 96, 98, 100] as const;
 
   constructor()
   {
-    setThemeFromHexColor('#ff0000');
+    setThemeFromHexColor(this.hexThemeColor);
     document.documentElement.classList.add('light');
   }
 }
