6118ab1e19f63f2fb4ff2fdb28ce049c6aa9bf1a
bengeendokter <benifit.arts@gmail.com>
2025-02-04 19:58:30 +0100

    Add m3-css-color-token-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { setThemeFromHexColor } from 'm3-css-color-token-generator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'm3-color-css';
+export class AppComponent
+{
+
+  constructor()
+  {
+    setThemeFromHexColor('#ff0000');
+    document.documentElement.classList.add('light');
+  }
 }
