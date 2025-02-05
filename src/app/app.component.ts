8d7ad2beb7458f6b8f270843525f3397597e5fa1
bengeendokter <benifit.arts@gmail.com>
2025-02-05 20:22:43 +0100

    Update innerHTML of palettes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { setThemeFromHexColor } from 'm3-css-color-token-generator';
 
+type LchColor = `oklch(${number} ${number} ${number})`;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -18,5 +20,49 @@ export class AppComponent
   {
     setThemeFromHexColor(this.hexThemeColor);
     document.documentElement.classList.add('light');
+  }
+
+  private isLchColor(color: string): color is LchColor
+  {
+    return color.startsWith('oklch');
+  }
+
+  ngAfterViewInit()
+  {
+    this.setPaletteInnerHtml();
+  }
+
+  private setPaletteInnerHtml()
+  {
+    this.PALETTES.forEach(palette =>
+    {
+      this.VALUES.forEach(value =>
+      {
+        const element = document.querySelector(`#${palette}${value}`);
+
+        if(!element)
+        {
+          console.warn(`Element with id ${palette}${value} not found!`);
+          return;
+        }
+
+        const compStyles = window.getComputedStyle(element);
+        const lchColor = compStyles.backgroundColor;
+
+        if(!this.isLchColor(lchColor))
+        {
+          console.warn(`Element with id ${palette}${value} has no LCH color!`);
+          return;
+        }
+
+        // get the L C and H values
+        const [l, c, h] = lchColor.match(/\d+(\.\d+)?/g)!.map(Number);
+
+        // round the values to 2 decimal places
+        const lchFixedDecimals = `${value.toString().padStart(2, '0')} L:${l.toFixed(2).padStart(5, '0')} C:${c.toFixed(2).padStart(5, '0')} H:${h.toFixed(2).padStart(5, '0')}`;
+
+        element.innerHTML = lchFixedDecimals;
+      });
+    });
   }
 }
